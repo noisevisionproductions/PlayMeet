6c20c01444e2e343e3b17847fac51094df0b60dd
Thomas <noisemonster29@gmail.com>
2023-11-07 01:03:05 +0100

    Zmiana wyglądu i funkcjonalności logowania oraz rejestracji, dodanie ważnego przycisku, który wraca do menu głownego - pozwoli to na dalsze rozwijanie aplikacji

--- app/src/main/java/com/example/zagrajmy/PostCreatingLogic.java
+++ app/src/main/java/com/example/zagrajmy/PostCreatingLogic.java
@@ -1,6 +1,7 @@
 package com.example.zagrajmy;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +10,9 @@ public class PostCreatingLogic extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.post_creating_user_info);
+
+        Button button = findViewById(R.id.backToMainMenu);
+        NavigationUtils.backToMainMenuButton(button, this);
 
     }
 
--- app/src/main/java/com/example/zagrajmy/PostsOfTheGames.java
+++ app/src/main/java/com/example/zagrajmy/PostsOfTheGames.java
@@ -1,6 +1,7 @@
 package com.example.zagrajmy;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,7 +12,9 @@ public class PostsOfTheGames extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_posts_list);
-        
+
+        Button button = findViewById(R.id.backToMainMenu);
+        NavigationUtils.backToMainMenuButton(button, this);
     }
 
 }
