51877b2289f0b3ad8683d0ce4a226fc029611984
Thomas <123601995+noisevisionproductions@users.noreply.github.com>
2024-01-13 22:04:50 +0100

    Update MySpinnerAdapter.java

--- app/src/main/java/com/noisevisionproductions/playmeet/Adapters/MySpinnerAdapter.java
+++ app/src/main/java/com/noisevisionproductions/playmeet/Adapters/MySpinnerAdapter.java
@@ -23,6 +23,7 @@ public class MySpinnerAdapter extends ArrayAdapter<String> {
     @NonNull
     @Override
     public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+// wygląd spinnera w domyślnej pozycji
         View view = super.getView(position, convertView, parent);
         TextView textView = view.findViewById(android.R.id.text1);
         textView.setGravity(Gravity.CENTER);
@@ -33,15 +34,18 @@ public class MySpinnerAdapter extends ArrayAdapter<String> {
     }
 
     public View getDropDownView(int position, View convertView, @NonNull ViewGroup parent) {
+// wygląd rozwijanego menu
         View view = super.getDropDownView(position, convertView, parent);
         TextView textView = view.findViewById(android.R.id.text1);
         textView.setGravity(Gravity.CENTER);
 
+// jeżeli spinner jest na indeksie 0 (1 pozycja na spinerze), to ustawiam tekst na przezroczysty tryb, z kolorem szarym oraz niemożliwym do kliknięcia, ponieważ w plikach .xml stworzonych przeze mnie, w których są dane, które spinner wyświetla, są na pierwszej pozycji informacje na temat kontentu spinnera, np. "Wybierz płeć"
         if (position == 0) {
             textView.setBackgroundColor(Color.TRANSPARENT);
             textView.setTextColor(Color.GRAY);
             textView.setEnabled(false);
         } else {
+// jeżeli spinner jest na innym indeksie niż 0, to ustawiam tekst na domyślny kolor
             textView.setTextColor(Color.BLACK);
         }
         return view;
